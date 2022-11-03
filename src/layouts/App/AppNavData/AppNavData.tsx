d9d77cbc2c4648a87465f24caa6ef64557cf8f39
cool29 <cool29@yandex.ru>
2022-11-04 02:56:17 +0500

    step 9

--- src/layouts/App/AppNavData/AppNavData.tsx
+++ src/layouts/App/AppNavData/AppNavData.tsx
@@ -1,12 +1,26 @@
 import { FC } from 'react';
+import { getRoutePath } from '@router/helpers';
+import { AppNavDataLink } from './AppNavDataLink';
 import styles from './AppNavData.module.scss';
 
 export const AppNavData: FC = () => {
   return (
     <nav className={styles.wrap}>
-      <ul>
-        <li>12131</li>
-        <li>1123123</li>
+      <ul className={styles.ul}>
+        <li>
+          <AppNavDataLink link={getRoutePath('main')} text={'Main'} />
+        </li>
+        <li>
+          <AppNavDataLink link={getRoutePath('about')} text={'About'} />
+        </li>
+        <li>
+          <AppNavDataLink
+            text={'Exit'}
+            onClick={() => {
+              alert('click on Exit');
+            }}
+          />
+        </li>
       </ul>
     </nav>
   );
